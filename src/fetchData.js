860bb9921146c67335e34adb9413ed1500d2d4d5
Kamilo2251 <camilo2251@gmail.com>
2023-05-11 12:00:00 -0500

    change url

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,4 +1,4 @@
 export function fetchData(user) {
-    return fetch(`https://api-platzi-profile.hop.sh/api_profile/AndresLaguilavo/${user}`)
+    return fetch(`https://api-platzi-profile.hop.sh/api_profile/${user}`)
       .then((response) => response.json());
   }
